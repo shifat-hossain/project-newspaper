b1ad0bd59c03bf9ec0fe511e66ff48080c8de7dd
Shifat Hossain <amithassan3229@gmail.com>
2025-05-16 21:53:05 +0600

    redesign pages

--- resources/views/admin/layout/footerlink.blade.php
+++ resources/views/admin/layout/footerlink.blade.php
@@ -1,36 +1,8 @@
-<!--begin:: Global Mandatory Vendors -->
-<script src="{{ asset('assets/vendors/general/jquery/dist/jquery.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/popper.js/dist/umd/popper.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/bootstrap/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/js-cookie/src/js.cookie.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/moment/min/moment.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/tooltip.js/dist/umd/tooltip.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/perfect-scrollbar/dist/perfect-scrollbar.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/sticky-js/dist/sticky.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/wnumb/wNumb.js') }}" type="text/javascript"></script>
-
-<!--end:: Global Mandatory Vendors -->
-
-<!--begin:: Global Optional Vendors -->
-<script src="{{ asset('assets/vendors/general/block-ui/jquery.blockUI.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/typeahead.js/dist/typeahead.bundle.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/handlebars/dist/handlebars.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/nouislider/distribute/nouislider.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/markdown/lib/markdown.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/toastr/build/toastr.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/raphael/raphael.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/morris.js/morris.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/vendors/general/chart.js/dist/Chart.bundle.js') }}" type="text/javascript"></script>
-
-<script src="{{ asset('assets/vendors/general/dompurify/dist/purify.js') }}" type="text/javascript"></script>
-
-<script src="{{ asset('assets/demo/default/base/scripts.bundle.js') }}" type="text/javascript"></script>
-<!--end::Page Vendors -->
-
-<!--begin::Page Scripts(used by this page) -->
-<script src="{{ asset('assets/app/custom/general/dashboard.js') }}" type="text/javascript"></script>
-
-<!--end::Page Scripts -->
-
-<!--begin::Global App Bundle(used by all pages) -->
-<script src="{{ asset('assets/app/bundle/app.bundle.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+</script>
+<script src="{{ asset('assets/js/scripts.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="{{ asset('assets/demo/chart-area-demo.js') }}"></script>
+<script src="{{ asset('assets/demo/chart-bar-demo.js') }}"></script>
+<script src="{{ asset('assets/js/datatables.min.js') }}"></script>
