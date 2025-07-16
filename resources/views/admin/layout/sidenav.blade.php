e070d1c6d7a9f3a312cdcd9c8af4a83c3bf86e72
Shifat Hossain <amithassan3229@gmail.com>
2025-07-17 01:03:12 +0600

    Update sidenav.blade.php

--- resources/views/admin/layout/sidenav.blade.php
+++ resources/views/admin/layout/sidenav.blade.php
@@ -45,14 +45,6 @@
                 Error
                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
             </a>
-            <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
-                data-bs-parent="#sidenavAccordionPages">
-                <nav class="sb-sidenav-menu-nested nav">
-                    <a class="nav-link" href="401.html">401 Page</a>
-                    <a class="nav-link" href="404.html">404 Page</a>
-                    <a class="nav-link" href="500.html">500 Page</a>
-                </nav>
-            </div>
         </nav>
     </div>
     <div class="sb-sidenav-menu-heading">App Config</div>
