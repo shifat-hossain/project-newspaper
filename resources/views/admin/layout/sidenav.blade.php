9aa047f44375e1ec1a376d68d8e8001e9b5f8bec
Shifat Hossain <amithassan3229@gmail.com>
2025-07-20 01:20:57 +0600

    Update sidenav.blade.php

--- resources/views/admin/layout/sidenav.blade.php
+++ resources/views/admin/layout/sidenav.blade.php
@@ -14,9 +14,7 @@
         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
     </a>
     <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-        <nav class="sb-sidenav-menu-nested nav">
-            <a class="nav-link" href="layout-static.html">Static Navigation</a>
-        </nav>
+        
     </div>
     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages"
         aria-expanded="false" aria-controls="collapsePages">
